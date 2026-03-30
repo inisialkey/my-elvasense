6379fa06d25ad626af19efe8facb003de142bb5d
inisialkey <okidorvel404@gmail.com>
2026-03-30 15:12:11 +0700

    refactor: update app icons, splash screens, and branding assets for production and staging environments

--- lib/core/resources/images.dart
+++ lib/core/resources/images.dart
@@ -3,16 +3,13 @@ class Images {
 
   static String icLauncher =
       const String.fromEnvironment('ENV', defaultValue: 'staging') == 'staging'
-          ? 'assets/images/ic_launcher_stg.png'
-          : 'assets/images/ic_launcher.png';
+      ? 'assets/images/ic_launcher_stg.png'
+      : 'assets/images/ic_logo_elvasense.png';
 
   static String icLauncherDark =
       const String.fromEnvironment('ENV', defaultValue: 'staging') == 'staging'
-          ? 'assets/images/ic_launcher_stg_dark.png'
-          : 'assets/images/ic_launcher_dark.png';
+      ? 'assets/images/ic_launcher_stg_dark.png'
+      : 'assets/images/ic_launcher_dark.png';
 
-  static String icLogo =
-      const String.fromEnvironment('ENV', defaultValue: 'staging') == 'staging'
-          ? 'assets/images/ic_logo_stg.png'
-          : 'assets/images/ic_logo.png';
+  static String icLogo = 'assets/images/ic_logo.png';
 }
--- lib/features/general/pages/splashscreen/splash_screen_page.dart
+++ lib/features/general/pages/splashscreen/splash_screen_page.dart
@@ -1,31 +1,50 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:go_router/go_router.dart';
 import 'package:myelvasense/core/core.dart';
-import 'package:myelvasense/features/general/general.dart';
 import 'package:myelvasense/utils/utils.dart';
 
 class SplashScreenPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) => Parent(
-      child: BlocListener<GeneralTokenCubit, GeneralTokenState>(
-        //coverage:ignore-start
-        listener: (context, state) => {
-          if (state is GeneralTokenStateSuccess)
-            {context.goNamed(Routes.root.name)}
-        },
-        //coverage:ignore-end
-        child: ColoredBox(
-          color: Theme.of(context).extension<LzyctColors>()!.background!,
-          child: Center(
-            child: Image.asset(
-              Theme.of(context).brightness == Brightness.dark
-                  ? Images.icLauncherDark
-                  : Images.icLauncher,
-              width: context.widthInPercent(55),
+    // child: BlocListener<GeneralTokenCubit, GeneralTokenState>(
+    //   //coverage:ignore-start
+    //   listener: (context, state) => {
+    //     if (state is GeneralTokenStateSuccess)
+    //       {context.goNamed(Routes.root.name)}
+    //   },
+    //coverage:ignore-end
+    child: Stack(
+      children: [
+        Container(
+          decoration: BoxDecoration(
+            image: DecorationImage(
+              image: AssetImage(
+                Theme.of(context).brightness == Brightness.dark
+                    ? 'assets/images/background_dark.jpeg'
+                    : 'assets/images/background.jpeg',
+              ),
+              fit: BoxFit.cover,
             ),
           ),
         ),
-      ),
-    );
+
+        Center(
+          child: Image.asset(Images.icLogo, width: context.widthInPercent(55)),
+        ),
+
+        Positioned(
+          bottom: 28 + MediaQuery.of(context).padding.bottom,
+          left: 0,
+          right: 0,
+          child: Text(
+            'Version 2.0',
+            textAlign: TextAlign.center,
+            style: Theme.of(context).textTheme.bodySmall?.copyWith(
+              fontWeight: FontWeight.w500,
+              color: Colors.white,
+            ),
+          ),
+        ),
+      ],
+    ),
+  );
 }
